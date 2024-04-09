a6290b2e43d9ff2cf09bdde1d9117004172d034b
rormains <romain.pro28@gmail.com>
2024-04-09 11:07:20 +0200

    feat: :sparkles: add real time processing with webcam images

---
+++ realtime.py
@@ -0,0 +1,72 @@
+import cv2
+from create_skin_mask import createSkinMask
+from match_img import classify
+from utils import draw_results_on_image
+
+# Initialiser la capture vidéo
+cap = cv2.VideoCapture(0)
+
+# Définir les coordonnées du rectangle (x, y, largeur, hauteur)
+# x et y définissent le coin supérieur gauche du rectangle
+x, y, w, h = 1300, 500, 600, 400
+
+while True:
+    # Capturer frame par frame
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Failed to grab frame")
+        break
+
+    # Appliquer l'effet miroir à l'image
+    frame = cv2.flip(frame, 1)
+
+    # Dessiner un rectangle autour de la région d'intérêt (ROI)
+    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    # Extraire la région d'intérêt (ROI) de l'image
+    roi = frame[y : y + h, x : x + w]
+
+    # Afficher la résolution de la frame
+    print(f"Resolution: {roi.shape[1]} x {roi.shape[0]}")
+
+    # Find the skin mask of the ROI
+    skin_mask = createSkinMask(
+        roi, histogram_file="histogram.plk", color_space="BGR", size=32
+    )
+
+    path_to_labels = "./data/labels"
+    chosen, liste_len_matching = classify(roi, path_to_labels, skin_mask)
+    sorted_results = sorted(
+        liste_len_matching.items(), key=lambda x: x[1], reverse=True
+    )
+
+    # Define position and size of the results box
+    results_top_left = (10, 10)
+    results_box_width = 150
+    results_box_height = 20 * len(sorted_results)  # 20 pixels per line of text
+    results_line_height = 20
+
+    # Draw the results on the frame
+    draw_results_on_image(
+        frame,
+        sorted_results,
+        results_top_left,
+        results_box_width,
+        results_box_height,
+        results_line_height,
+    )
+
+    # Afficher la frame avec le rectangle
+    cv2.imshow("Webcam Live", frame)
+
+    # Afficher le masque de peau
+    cv2.imshow("Skin Mask", skin_mask)
+
+    # Appuyez sur 'q' pour quitter la boucle
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+# Lorsque tout est fait, libérer la capture
+cap.release()
+cv2.destroyAllWindows()
--- utils.py
+++ utils.py
@@ -15,3 +15,36 @@ def is_image_file(filename):
     # Liste des extensions de fichiers d'image valides
     valid_extensions = [".jpg", ".jpeg", ".png", ".bmp", ".tif", ".tiff"]
     return any(filename.lower().endswith(ext) for ext in valid_extensions)
+
+
+def draw_results_on_image(
+    frame, results, top_left_corner, box_width, box_height, line_height
+):
+    # Draw a filled white rectangle
+    cv2.rectangle(
+        frame,
+        top_left_corner,
+        (top_left_corner[0] + box_width, top_left_corner[1] + box_height),
+        (255, 255, 255),
+        cv2.FILLED,
+    )
+
+    # Set text properties
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 0.6
+    font_thickness = 2
+    text_color = (0, 0, 0)
+
+    # Initialize the position for the first line of text
+    text_position = (top_left_corner[0] + 10, top_left_corner[1] + line_height)
+
+    # Draw each result as a new line of text
+    for label, score in results:
+        text = (
+            f"{label.split('.')[0]} : {score}"  # Remove the file extension from label
+        )
+        cv2.putText(
+            frame, text, text_position, font, font_scale, text_color, font_thickness
+        )
+        # Move to the position of the next line
+        text_position = (text_position[0], text_position[1] + line_height)
